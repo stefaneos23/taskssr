a1b000209f01e311fa5a0cbeeb36b56a58f284b1
Stefan Rahlitchi <stefan.rahlitchi@eos-it.ro>
2023-04-24 09:47:02 +0000

    Task1+2

---
+++ tasks-service/src/main/java/com/example/tasksservices/TasksServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasksservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksServicesApplication.class, args);
+    }
+
+}
---
+++ tasks-service/src/test/java/com/example/tasksservices/TasksServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasksservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
